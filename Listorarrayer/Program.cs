7cbde3bdbbb9e50cf35c3ba8227094b06e16d29f
Vincent Sabrie <vincent.sabrie@elev.ga.ntig.se>
2024-11-18 09:03:21 +0100

    5 leksaker övning 2

--- Listorarrayer/Program.cs
+++ Listorarrayer/Program.cs
@@ -1,2 +1,7 @@
 ﻿List<string> bois = ["Woody", "Buzz Lightyear", "Mr. Potato", "Tågleksak", "Docka"];
 
+for (int i = 0; i < bois.Count; i++)
+{
+   System.Console.WriteLine($"{bois [i]}"); 
+}
+Console.ReadLine();

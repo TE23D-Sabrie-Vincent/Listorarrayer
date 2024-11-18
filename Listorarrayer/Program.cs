da4433cf4a1f8f4bab2e32a331ab36a7cf292434
Vincent Sabrie <vincent.sabrie@elev.ga.ntig.se>
2024-11-18 09:07:14 +0100

    Klasskamrater Övning 3

--- Listorarrayer/Program.cs
+++ Listorarrayer/Program.cs
@@ -1,7 +1,10 @@
-﻿List<string> bois = ["Woody", "Buzz Lightyear", "Mr. Potato", "Tågleksak", "Docka"];
+﻿// List<string> bois = ["Woody", "Buzz Lightyear", "Mr. Potato", "Tågleksak", "Docka"];
 
-for (int i = 0; i < bois.Count; i++)
-{
-   System.Console.WriteLine($"{bois [i]}"); 
-}
+// for (int i = 0; i < bois.Count; i++)
+// {
+//    System.Console.WriteLine($"{bois [i]}"); 
+// }
+
+List<string> klasskamrater = ["Simon", "Theodor", "Richard", "Konstantin", "Kevin"];
+
 Console.ReadLine();

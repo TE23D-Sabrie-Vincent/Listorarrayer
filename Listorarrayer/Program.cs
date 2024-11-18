cafaca16a18cafe2409aad92b40bc29e81bf9c9c
Vincent Sabrie <vincent.sabrie@elev.ga.ntig.se>
2024-11-18 09:32:03 +0100

    Övning 6 cities

--- Listorarrayer/Program.cs
+++ Listorarrayer/Program.cs
@@ -7,20 +7,22 @@
 
 // List<string> klasskamrater = ["Simon", "Theodor", "Richard", "Konstantin", "Kevin"];
 
-List<string> klasskamrater = ["Simon", "Theodor", "Richard", "Konstantin", "Kevin"];
+// List<string> klasskamrater = ["Simon", "Theodor", "Richard", "Konstantin", "Kevin"];
 
-List<string> leksaker = ["Woody", "Buzz Lightyear", "Mr. Potato", "Tågleksak", "Docka"];
+// List<string> leksaker = ["Woody", "Buzz Lightyear", "Mr. Potato", "Tågleksak", "Docka"];
 
-List<string> nummer = ["1", "3", "5", "7", "9"];
+// List<string> nummer = ["1", "3", "5", "7", "9"];
 
-// for (int i = 0; i < nummer.Count; i++)
+// // for (int i = 0; i < nummer.Count; i++)
+// // {
+// //     nummer[i] = Random.Shared.Next(1, 10).ToString();
+// // }
+// // Ignorerar Random nummer på betyget, jag fick hjälp med det
+
+// for (int i = 0; i < klasskamrater.Count; i++)
 // {
-//     nummer[i] = Random.Shared.Next(1, 10).ToString();
+//     System.Console.WriteLine($"{klasskamrater[i]} ger {leksaker[i]} betyget {nummer[i]}");
 // }
-// Ignorerar Random nummer på betyget, jag fick hjälp med det
 
-for (int i = 0; i < klasskamrater.Count; i++)
-{
-    System.Console.WriteLine($"{klasskamrater[i]} ger {leksaker[i]} betyget {nummer[i]}");
-}
-Console.ReadLine();
+List<string> cities = [];
+
